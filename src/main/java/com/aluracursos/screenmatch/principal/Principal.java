f0b88df7243c754e45aff89dc60332c91aeb0fdb
jcmunav63 <jcmunav63@gmail.com>
2024-11-04 20:56:28 -0600

    Crear clase Episodio. Usar streams y lambdas para filtrar, ordenar, y buscar episodios generando listas de resultados.

--- src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -3,12 +3,17 @@ package com.aluracursos.screenmatch.principal;
 import com.aluracursos.screenmatch.model.DatosEpisodio;
 import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.model.DatosTemporada;
+import com.aluracursos.screenmatch.model.Episodio;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner teclado = new Scanner(System.in);
@@ -48,6 +53,50 @@ public class Principal {
             System.out.println(" ");
         }
         System.out.println(" =============");
+        System.out.println(" ");
+        System.out.println("Lista de episodios (sólo el nombre)");
+        System.out.println("-----------------------------------");
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        // Convertir toda la información de episodios a una lista del tipo DatosEpisodio
+        List<DatosEpisodio> datosEpisodios = temporadas.stream()
+                .flatMap(t-> t.episodios().stream())
+                        .collect(Collectors.toList());
+
+        // Hallar el top 5 de episodios
+        System.out.println(" ");
+        System.out.println("Lista del top 5 de episodios de todas las temporadas...");
+        System.out.println("----------------------------------------------------");
+        datosEpisodios.stream()
+                .filter(e-> !e.evaluacion().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DatosEpisodio::evaluacion).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+        // Convirtiendo los datos a una Lista del tipo Episodio
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t-> t.episodios().stream()
+                        .map(d-> new Episodio(t.numero(),d)))
+                .collect(Collectors.toList());
+        System.out.println(" ");
+        System.out.println("Temporadas y Episodios");
+        System.out.println("----------------------");
+        episodios.forEach(System.out::println);
+
+        // Búsqueda de episodios a partir de un año específico
+        System.out.println("Indica el año a partir del cual desea buscar episodios...");
+        var fecha = teclado.nextInt();
+        teclado.nextLine();
+
+        LocalDate fechaBusqueda = LocalDate.of(fecha, 1, 1);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        episodios.stream()
+                .filter(e-> e.getFechaDeLanzamiento() != null &&
+                        e.getFechaDeLanzamiento().isAfter(fechaBusqueda))
+                .forEach(e-> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                                " | Episodio: " + e.getTitulo() +
+                                " | Fecha lanz.: " + e.getFechaDeLanzamiento().format(dtf)
+                ));
     }
 }
